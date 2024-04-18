602580e97704d95543537a98009f13d51229288a
89 <eighty9nine@gmail.com>
2024-04-18 10:37:22 +0300

    Remove export button

--- resources/views/components/actions-panel.blade.php
+++ resources/views/components/actions-panel.blade.php
@@ -4,7 +4,7 @@
         <div class="flex-grow">
             <p class="text-xl">Filters</p>
         </div>
-        <x-filament::dropdown>
+        {{-- <x-filament::dropdown>
             <x-slot name="trigger">
                 <x-filament::button>
                     Export
@@ -12,19 +12,19 @@
             </x-slot>
 
             <x-filament::dropdown.list>
-                {{-- <x-filament::dropdown.list.item wire:click="exportToPdf">
+                <x-filament::dropdown.list.item wire:click="exportToPdf">
                     To PDF
                 </x-filament::dropdown.list.item>
 
                 <x-filament::dropdown.list.item @click="$exportToExcel()">
                     To Excel
-                </x-filament::dropdown.list.item> --}}
+                </x-filament::dropdown.list.item>
 
                 <x-filament::dropdown.list.item @click="$printReport()">
                     Print
                 </x-filament::dropdown.list.item>
             </x-filament::dropdown.list>
-        </x-filament::dropdown>
+        </x-filament::dropdown> --}}
     </div>
     <div>
         <x-filament-panels::form :wire:key="$this->getId() . '.forms.' . $this->getFormStatePath()"
